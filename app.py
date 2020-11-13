a974fc4ae087a04b2a1181d56d18ad8352021cb9
jiroken25 <k.hirota0205@gmail.com>
2020-11-14 02:23:33 +0800

    data table

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, render_template
 import numpy as np
 from tensorflow.keras.models import model_from_json
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+
 
 def model_predict(x):
     model = model_from_json(open('covid_model.json', 'r').read())
@@ -17,7 +21,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(app)
+
+    result = db.session.execute('SELECT "Country","Population","Death","death/population","Health_Spending","Hospital_bed","Hospital_stay","Life_expectancy","Unemployment_rate","Obesity_rate","transporation","poverty_rate",	"science_score" FROM country_name inner join covid_data on country_name.index = covid_data.country_id inner join health_spending on country_name.index = health_spending.country_id inner join hospital_bed on country_name.index = hospital_bed.country_id inner join hospital_stay on country_name.index = hospital_stay.country_id inner join life_expectancy on country_name.index = life_expectancy.country_id inner join unemployment on country_name.index = unemployment.country_id inner join obesity on country_name.index = obesity.country_id inner join transporation on country_name.index = transporation.country_id inner join poverty on country_name.index = poverty.country_id inner join "Science_score" on country_name.index = "Science_score".country_id')
+    data = json.dumps([dict(r) for r in result])
+	
+    return render_template('index.html',data=data)
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
